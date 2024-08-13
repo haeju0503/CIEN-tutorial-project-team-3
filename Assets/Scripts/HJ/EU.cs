5bb8afbf5001d2938b8ca5cffce9d518478f3ade
haeju0503 <haeju050@gmail.com>
2024-08-14 01:00:56 +0900

    EU 스크립트 개선
    이제 레벨업 버튼 누르면 레벨업 설명 (EU Desc) 텍스트 업데이트 함

--- Assets/Scripts/HJ/EU.cs
+++ Assets/Scripts/HJ/EU.cs
@@ -5,8 +5,14 @@ using UnityEngine.UI;
 
 public class EU : MonoBehaviour
 {
+    //Decs타입하고 버튼 타입, 이름 타입 각각 따로 만들어보면? 
+
     public EUData data;
     public int level;
+
+    public enum EUObjectType { name, decs, upButton, downButton}
+
+    public EUObjectType type;
 
     Text textName;
     Text textDesc;
@@ -15,30 +21,59 @@ public class EU : MonoBehaviour
 
     private void Awake()
     {
-        Text[] texts = GetComponentsInChildren<Text>();
-        textName = texts[0];
-        textDesc = texts[1];
-        textUpgrade = texts[2];
-        textDowngrade = texts[3];
-        textName.text = data.euName;
+        level = -1;
+        LevelUp();
     }
-    private void OnEnable()
+
+    public void LevelUp()
     {
-        switch (data.euType)
+        if (level == data.maxLevel)
+            return;
+
+        level++;
+
+        Text[] texts = GetComponentsInChildren<Text>();
+
+        switch (type)
         {
-            case EUData.EUType.IntEU:
-                textDesc.text = string.Format(data.euDesc, data.intIncrement[level] * 100);
+            case EUObjectType.name:
+                textName = texts[0];
+                textName.text = data.euName;
                 break;
-            case EUData.EUType.FloatEU:
-                textDesc.text = string.Format(data.euDesc, data.floatIncrement[level] * 100);
+            case EUObjectType.decs:
+                switch (data.euType)
+                {
+                    case EUData.EUType.IntEU:
+                        textDesc = texts[0];
+                        int sum = 0;
+                        for (int index = 0; index < level; index++)
+                        {
+                            sum += data.intIncrement[index];
+                        }
+                        if (level == data.maxLevel)
+                            textDesc.text = string.Format("최고레벨입니다!\n현재 최대체력 증가량 : {0}", sum);
+                        else
+                            textDesc.text = string.Format(data.euDesc, data.intIncrement[level], sum); //이떄만 버튼클릭 소리나게 하고 싶은데...
+                        break;
+                    case EUData.EUType.FloatEU:
+
+                        break;
+                    case EUData.EUType.SpecialEU:
+
+                        break;
+                    default:
+
+                        break;
+                }
                 break;
-            case EUData.EUType.SpecialEU:
+            case EUObjectType.upButton:
+
+                break;
+            case EUObjectType.downButton:
 
                 break;
             default:
-                
                 break;
         }
-        //작동 X 하나하나하는게 나을지도..
     }
 }
